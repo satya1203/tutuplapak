1240d5db192a61cc1a80e1b288be781100e457ba
satya1203 <christiansatyadharma.csd@gmail.com>
2021-11-19 11:08:05 +0700

    func login

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// function callREST(url, method) {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//           document.getElementById("response").innerHTML = this.responseText;
+//       }
+//   };
+//   xhttp.open(method, url, true);
+//   xhttp.send();
+// }
+ 
 new Vue({
   router, 
   render: h => h(App),
